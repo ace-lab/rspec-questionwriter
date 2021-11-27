d206af9ce75117a2b4bfc0b6beed48945670932a
nelson lojo <nalsonklojo@gmail.com>
2021-11-27 15:32:13 -0800

    parson's script subroutine working

--- suites_gen.py
+++ suites_gen.py
@@ -5,8 +5,7 @@ from sys import argv
 import os
 from uuid import uuid4
 
-base_info_json = f"""
-{{
+base_info_json = f"""{{
     "uuid": "{uuid4()}",
     "title": "",
     "topic": "",
@@ -30,21 +29,15 @@ def write_to(filename: str, content: str) -> None:
     with open(filename, 'w') as file:
         file.write(content)
 
-def make_parson_source(q_root: str, prompt: str, solution: str) -> None:
+def make_parson_source(q_root: str, prompt: str, solution: str, q_name: str) -> None:
     """Make source.py for the Faded-Parson's problem system"""
-    with open("source.py" ,'w') as source:
-        source.write(f"\"\"\"{prompt}\"\"\"")
-        source.write('\n\n')
-        source.write(f"## import {q_root}/info.json as info.json ##\n")
-        source.write('\n')
-        source.write(solution)
-        source.write('\n')
-
-def generate_parsons_component() -> None:
-    """Generate the necessary components for displaying the parson's problem to the student"""
-    # TODO: run logan's tool
-    pass
-
+    source = f"""\"\"\"{prompt}\"\"\"
+\n
+{solution}
+\n
+"""
+    write_to(f"{argv[2]}/{q_name}.py", source)
+        
 def generate_mutations(q_root: str, mutations: Dict):
 
     def overlaps(intervals: Tuple[int, int]) -> bool:
@@ -59,14 +52,17 @@ def generate_mutations(q_root: str, mutations: Dict):
 
         for file, mutations in files.items():
             line = 1
-            locations = list(map( # basically 'num1-num2' -> { (num1,num2): 'num1-num2', ...}
-                lambda s, e: (int(s), int(e)),
-                map(
-                    lambda i: i.split('-'), 
-                    file.keys()
-                )
-            ))
-            locations = sorted(locations, key=(lambda e: e[0]))
+
+
+            locations = map(
+                lambda i: i.split('-'), 
+                mutations.keys()
+            )
+            locations = map( # basically 'num1-num2' -> { (num1,num2): 'num1-num2', ...}
+                lambda intv: (int(intv[0]), int(intv[1])),
+                locations
+            )
+            locations = sorted(list(locations), key=(lambda e: e[0]))
 
             if overlaps(locations):
                 clean_up()
@@ -90,23 +86,26 @@ def generate_mutations(q_root: str, mutations: Dict):
                     out.writelines(inp.readlines())
 
 def write_solution(q_root: str, solution: str) -> None:
-    """Generate suites/solution/_submission_file using the provided solution"""
-    with open(f"{q_root}/suites/solution/_submission_file", 'w') as submission_file:
-        for line in solution.split('\n'):
-            submission_file.writeline(line.replace('?', '') + '\n')
+    """Generate tests/solution/_submission_file using the provided solution"""
+    write_to(f"{q_root}/tests/solution/_submission_file", solution.replace('?', ''))
 
 def write_metadata(q_root: str, submit_to: str) -> None:
-    with open(f"{q_root}/suites/meta.json", 'w') as meta:
-        meta.write(json_dumps({ "submission_file" : submit_to, "submission_root" : "" }))
+    write_to(
+        f"{q_root}/tests/meta.json", 
+        json_dumps({ "submission_file" : submit_to, "submission_root" : "" })
+    )
 
 def clean_up() -> None:
     # TODO: remove all files and make it look like we were never here
     exit(1)
 
 if __name__ == "__main__":
-    q_root = argv[1]
+    # q_root = argv[1]
+    yaml_file = argv[1]
+    q_name = yaml_file[:yaml_file.index('.')]
+    q_root = f"{argv[2]}/{q_name}/"
 
-    content: Dict[str, Any] = yaml.safe_load(open(f"{q_root}/question.yaml"))
+    content: Dict[str, Any] = yaml.safe_load(open(f"{yaml_file}"))
 
     assert "solution" in content.keys(), f"`solution:` is a required field in question.yaml"
     assert "common" in content.keys(), f"`common:` is a required field in question.yaml"
@@ -115,13 +114,13 @@ if __name__ == "__main__":
 
     prompt: str = content.get("prompt", "")
     solution: str = content["solution"]
-    # TODO: make info.json
-    # write_to("info.json", base_info_json)
-    make_parson_source(q_root, prompt, solution)
+    make_parson_source(q_root, prompt, solution, q_name)
     
     # should include ans.py, question.html
-    generate_parsons_component() 
+    os.system(f"/usr/bin/python3 generate_fpp.py {argv[2]}/{q_name}.py")
 
+    print("fpp done")
+    write_to(f"{q_root}/info.json", base_info_json)
 
     safe_mkdir(f"{q_root}/tests")
 
