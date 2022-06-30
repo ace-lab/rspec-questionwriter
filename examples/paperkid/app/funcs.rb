bd6681dc939233943c36dab914209edb0ee52ad9
mverdicchio <verdicchio@gmail.com>
2022-06-30 15:34:23 -0400

    organized just for wallet

--- examples/paperkid/app/funcs.rb
+++ examples/paperkid/app/funcs.rb
@@ -2,10 +2,9 @@ class InsufficientFundsError < StandardError
 end
 
 class Wallet
-    attr_reader :cash # not attr_accessor
-      # need a way to add money for Wallet tests
+    attr_reader :cash
     def initialize(amount)
-      raise ArgumentError if amount <= 0
+      raise ArgumentError if amount < 0
       @cash = amount
     end
     def withdraw(amount)
@@ -15,24 +14,3 @@ class Wallet
        amount
     end
   end
-
-class Customer
-  # no # attr_accessor :wallet
-  def initialize(wallet)
-    @wallet = wallet
-  end
-  # behavior delegation
-  def pay(amount)
-    @wallet.withdraw(amount)
-  end
-end
-
-class PaperKid
-  attr_reader :collected_amount
-  def initialize
-    @collected_amount = 0
-  end
-  def collect_money(customer, due_amount)
-    @collected_amount += customer.pay(due_amount)
-  end
-end
--- examples/paperkid/app/spec/funcs_spec.rb
+++ examples/paperkid/app/spec/funcs_spec.rb
@@ -6,76 +6,40 @@ require_relative '../funcs.rb'
 describe 'Wallet Tests' do
     describe 'constructor' do
         it 'should reject <0 money' do
-            expect { Wallet.new(-4) }.to raise_error(ArgumentError)
+            bad_amount = -100
+            expect { Wallet.new(bad_amount) }.to raise_error(ArgumentError)
         end
     end
     describe 'getter setter' do
-        before(:each) { @wallet = Wallet.new(20) }
         it 'should have cash set by constructor' do
-            expect(@wallet.cash).to eq(20)
+            amount = 20
+            wallet = Wallet.new(amount)
+            expect(wallet.cash).to eq(20)
         end
         it 'should not have a cash setter method' do
-            expect{ @wallet.cash = 100 }.to raise_error(NoMethodError)
+            wallet = Wallet.new(10)
+            expect{ wallet.cash = 100 }.to raise_error(NoMethodError)
         end
     end
     describe 'withdraw' do
-        before(:each) { @wallet = Wallet.new(20) }
         it 'should remove valid amount' do
-            expect{@wallet.withdraw(10)}.not_to raise_error
-            expect(@wallet.cash).to eq(10)
+            amount = 20
+            amount_to_remove = 10
+            wallet = Wallet.new(amount)
+            expect{ wallet.withdraw(amount_to_remove) }.not_to raise_error
+            expect(wallet.cash).to eq(amount - amount_to_remove)
         end
         it 'should not withdraw too much' do
-            expect{ @wallet.withdraw(100) }.to raise_error(InsufficientFundsError)
-            expect( @wallet.cash ).to eq(20)
+            amount = 20
+            wallet = Wallet.new(amount)
+            expect{ wallet.withdraw(amount + 1) }.to raise_error(InsufficientFundsError)
+            expect( wallet.cash ).to eq(amount)
         end
         it 'should not withdraw negative money' do
-            expect{ @wallet.withdraw(-4) }.to raise_error(ArgumentError)
-            expect( @wallet.cash ).to eq(20)
-        end
-    end
-end
-
-describe 'Customer Tests' do
-    describe 'constructor' do
-        it 'should create a customer given a wallet' do
-            wallet_double = double("Wallet")
-            expect { customer = Customer.new(wallet_double) }.not_to raise_error
-        end
-    end
-
-    describe 'paying up' do
-        # customer has a wallet, not cash.
-        # we don't test the wallet here. hard to resist
-        it 'should call withdraw in Wallet' do
-            wallet_double = double("Wallet")
-            @customer = Customer.new(wallet_double)
-            expect(wallet_double).to receive(:withdraw)
-            @customer.pay(20)
-        end
-    end
-end
-
-describe 'PaperKid tests' do
-    describe 'constructor' do
-        it 'should instantiate' do
-            expect{ pk = PaperKid.new }.not_to raise_error
-        end
-    end
-    describe 'money collection' do
-        before(:each) do
-            # @collected_money must be initialized to increment later
-            @pk = PaperKid.new
-            @customer_dbl = double("Customer")
-        end
-        it 'should call pay method of Customer' do
-            # dbl must return or @collected_money increment fails
-            expect(@customer_dbl).to receive(:pay).with(10).and_return(10)
-            @pk.collect_money(@customer_dbl, 10)
-        end
-        it 'should increment collected amount' do
-            allow(@customer_dbl).to receive(:pay).with(10).and_return(10)
-            @pk.collect_money(@customer_dbl, 10)
-            expect(@pk.collected_amount).to eq(10)
+            amount = 20
+            wallet = Wallet.new(amount)
+            expect{ wallet.withdraw(-1 * amount) }.to raise_error(ArgumentError)
+            expect( wallet.cash ).to eq(amount)
         end
     end
 end
